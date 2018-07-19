c55e4872a5f006822fc796c46997a7faf9f55ad1
theonlyedmund <email.edmund@gmail.com>
2018-07-18 22:06:14 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
+
+var bodyParser = require('body-parser');
     
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
+app.use(bodyParser.json()); // for parsing application/json
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
