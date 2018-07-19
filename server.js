4d679b9a2365f406c9225d81f192dae8da78b3de
theonlyedmund <email.edmund@gmail.com>
2018-07-18 22:00:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
-app.use(express.bodyParser());
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
